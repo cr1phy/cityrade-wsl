2a01e2f46dd7ee269ca4c2b6009a8a65b5e758dc
Sergey Krifov <cr1phy@mail.ru>
2025-11-07 08:18:58 +0400

    changes

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpServer, Responder};
+use std::io;
+
+#[get("/")]
+async fn index() -> impl Responder {
+    "Hello! It's 8:12, and your city grows up every day! Good job!"
+}
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    let server = HttpServer::new(move || App::new().service(index));
+    server.bind("0.0.0.0:8080")?.run().await?;
+    Ok(())
 }
